593844532e56eb732b38781e40d44e4599f5484d
Beatriz-Peralta <114191631+Beatriz-Peralta@users.noreply.github.com>
2023-03-09 19:07:04 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,15 +2,12 @@ var Ball, database;
 var position;
 
 function setup() {
-    
-    console.log(database);
+    database = firebise.database().
     createCanvas(500, 500);
-
     Ball = createSprite (250,250,10,10);
     Ball.shapeColor = "red"; 
-
-    
-    
+    var BallPosition = database.ref("bola/posicao"); // ref = referencia. Lê o no que sera ligado a posicao da bola
+    BallPosition.on("valeu", readPosition, showError); //value = valor. Recebe o valor ligado a posicao 
 }
 
 function draw() {
@@ -28,14 +25,16 @@ function draw() {
 }
 
 function writePosition(x, y) {
-    
-    
-    
+    database.ref("bola/posicao").set({ //set = troca, mudanca. Vai mudar o valor da posicao X e Y conforme se move
+         x: position.x + x, //atualiza a posicao de X
+         y:position.y + y, //atualiza a posicao de Y
+      });
 }
 
 function readPosition(data) {
-    
-    
+    position = data.val(); //valor relacionado a posicao no banco de dados
+    Ball.x = position.x;
+    Ball.y = position.y;
 }
 
 function showError() {
